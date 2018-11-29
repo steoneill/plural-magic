2848528812f64877a99a373e52a71ea352e87797
Ste <ste.tpe@gmail.com>
2018-11-29 22:20:03 +0000

    Added further support for node spec

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.exports = function plural(count, item) {
+"use strict";
+
+/**
+ * Turn your item into items, shoe into shoes etc.
+ * @param {number} count
+ * @param {string} item
+ * @return {string}
+ */
+
+module.exports = function(count, item) {
   return `${item}${count > 1 && `s`}`;
 };
